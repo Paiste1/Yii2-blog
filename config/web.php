bf502d0a8a12e06d0080acfec4eed39d8f12a604
Paiste1 <alexpaiste.ap@gmail.com>
2020-02-10 13:58:49 +0800

    настроил чпу

--- config/web.php
+++ config/web.php
@@ -22,6 +22,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Rm-ml2BJ3_MVlIpDpo_CL-ZYHHlKAUD8',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -50,14 +51,18 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                'page/<page:\d+>' => 'post/index',
+                '/' => 'post/index',
+                'post/<id:\d+>' => 'post/view',
+                'category/<alias:[a-zA-Z0-9-]+>' => 'category/view',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
--- controllers/PostController.php
+++ controllers/PostController.php
@@ -11,9 +11,15 @@ class PostController extends Controller
     public function actionIndex()
     {
         //$posts = Post::find()->all();
-        $query = Post::find();
-        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 4]); // считаем количество записей, и выводим по 4 на страницу
+        $query = Post::find()->with('category'); // выполняем жадную загрузку, достаем все категории за один раз
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 4, 'forcePageParam' => false, 'pageSizeParam' => false]); // считаем количество записей, и выводим по 4 на страницу
         $posts = $query->offset($pages->offset)->limit($pages->limit)->all();
         return $this->render('index', compact('posts', 'pages'));
+    }
+
+    public function actionView($id)
+    {
+        var_dump($id);
+        die;
     }
 }
